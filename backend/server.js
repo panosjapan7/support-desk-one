9e5d1855cacf58d3b9aa7010c7360961359fa756
panosjapan7 <panosjapan@gmail.com>
2022-06-13 15:05:43 +0200

    Error and Exception Handling

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require("express"); // Bring in express
 const dotenv = require("dotenv").config(); // Bring in dotenv
+const {errorHandler} = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 5000;
 const app = express(); // Initialize express
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
     res.status(200).json({message: "Welcome to the Support Desk API"});
@@ -9,5 +13,6 @@ app.get("/", (req, res) => {
 
 //Routes
 app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
