b0d82805c0d30deaec4f75fb7e92d4cdbf5c8a59
panosjapan7 <panosjapan@gmail.com>
2022-06-13 16:18:27 +0200

    Connect to MongoDB db

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require("express"); // Bring in express
+const colors = require("colors");
 const dotenv = require("dotenv").config(); // Bring in dotenv
-const {errorHandler} = require("./middleware/errorMiddleware");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db"); 
 const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB database
+connectDB()
+
 const app = express(); // Initialize express
 
 app.use(express.json());
