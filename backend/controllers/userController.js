4205491443556250e3dfba14b2f3eba4b894464c
panosjapan7 <panosjapan@gmail.com>
2022-06-14 14:11:06 +0200

    Protect Routes and Authentication, set up Frontend React

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,6 +77,18 @@ const loginUser = asyncHandler(
     }
 ) 
 
+// @desc Get current user
+// @route /api/users/me
+// @access Private
+const getMe = asyncHandler(async (req, res) => {
+    const user = {
+        id: req.user._id,
+        email: req.user.email,
+        name: req.user.name,
+    }
+    res.status(200).json(user);
+})
+
 // Generate token
 const generateToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET, { expiresIn: "30d"});
@@ -86,4 +98,5 @@ const generateToken = (id) => {
 module.exports = {
     registerUser,
     loginUser,
+    getMe,
 }
