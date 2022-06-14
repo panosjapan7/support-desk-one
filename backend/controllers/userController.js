82d8eaea8a117e34df64fae76f5fd8615b11f4b7
panosjapan7 <panosjapan@gmail.com>
2022-06-14 10:13:24 +0200

    Login and Create JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/userModel");
 
@@ -41,6 +42,7 @@ const registerUser = asyncHandler(
                 _id: user._id,
                 name: user.name,
                 email: user.email,
+                token: generateToken(user._id),
             })
         }
         else {
@@ -55,9 +57,31 @@ const registerUser = asyncHandler(
 // @access Public
 const loginUser = asyncHandler(
     async (req, res) => {
-        res.send("Login Route");
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email});
+
+        // Check user and hash and plain-text passwords match
+        if(user && (await bcrypt.compare(password, user.password))) {
+            res.status(200).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                token: generateToken(user._id),
+            })
+        }
+        else {
+            res.status(401);
+            throw new Error("Invalid credentials");
+        }
     }
 ) 
+
+// Generate token
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, { expiresIn: "30d"});
+}
+
 
 module.exports = {
     registerUser,
