2067f670aad7bfac80002dd2ae77b72428466c34
panosjapan7 <panosjapan@gmail.com>
2022-06-15 12:53:52 +0200

    Hook Register Form to Redux

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { FaSignInAlt } from "react-icons/fa";
 import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+import { login } from "../features/auth/authSlice";
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -8,6 +10,9 @@ function Login() {
     password: "",
   })
   const {email, password} = formData;
+
+  const dispatch = useDispatch();
+  const {user, isLoading, isSuccess, message} = useSelector((state) => state.auth);
 
   const onChange = (e) => {
     setFormData((prevState) => (
@@ -20,6 +25,9 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    
+    const userData = { email, password};
+    dispatch(login(userData));
   }
 
   return (
