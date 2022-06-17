b5b2de6c333f6010dec77a36b8c38f1da2107c04
panosjapan7 <panosjapan@gmail.com>
2022-06-17 13:50:49 +0200

    New Ticket Form

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
         <h1>What do you need help with?</h1>
         <p>Please choose from an option below</p>
       </section>
-      <Link to="/create-ticket" className="btn btn-reverse btn-block">
+      <Link to="/new-ticket" className="btn btn-reverse btn-block">
         <FaQuestionCircle /> Create New Ticket
       </Link>
       <Link to="/ticket" className="btn btn-block">
